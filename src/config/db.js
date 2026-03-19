994314e160aa2b5b7cc2d22b36ef7cf3d165144e
avhis-hek7 <sharmaavishek679@gmail.com>
2026-03-19 12:34:56 +0545

    add validation whether the mongo uri exists in env variables or not

--- src/config/db.js
+++ src/config/db.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 async function connectDb(){
     try {
+        if(!process.env.MONGO_URI){
+            throw new Error("Mongo URI is not defined in environmnent variables.");
+        }
         await mongoose.connect(process.env.MONGO_URI)
         console.log("Db is connect to server");
     } catch (error) {
