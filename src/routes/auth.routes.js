36480d1120e34cb858498195bd972ed3bc201504
avhis-hek7 <sharmaavishek679@gmail.com>
2026-03-19 18:06:03 +0545

    error configure successfully

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,7 +5,7 @@ const authContoller = require('../controllers/auth.controller');
 const router = express.Router();
 router.post('/register',authContoller.userRegisterController);
 
-router.post('/get-me',authContoller.getMeUseUserController);
+router.get('/get-me',authContoller.getMeUseUserController);
 
 
 module.exports = router;
