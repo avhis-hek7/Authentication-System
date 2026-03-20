7504ded874883d3e789b8a50f663be95150a73bf
avhis-hek7 <sharmaavishek679@gmail.com>
2026-03-20 14:33:52 +0545

    create a sessopn schema

---
+++ src/models/session.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const sessionSchema = new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"users",
+        required:[true, "User is required"]
+    },
+    refreshToken:{
+        type:String,
+        required:[true, "Refresh Token is required"]
+    },
+    ip:{
+        type:String,
+        required:[true, "IP Token is required"]
+    },
+    userAgent:{
+        type:String,
+        required:[true, "User Agent Token is required"]
+    },
+    revoked:{
+        type:Boolean,
+        default: false
+    }
+
+},{timestamps:true})
+
+const sessionModel = mongoose.model("sessions", sessionSchema);
+module.exports = sessionModel;
